10be924a1200cc59359d0609a0c501392cd80cf0
Antonio081014 <antonio081014@users.noreply.github.com>
2017-04-19 17:09:38 -0700

    Update AExtUIImage.swift

--- AExtUIImage.swift
+++ AExtUIImage.swift
@@ -1,6 +1,6 @@
 extension UIImage {    
     convenience init(view: UIView) {
-        UIGraphicsBeginImageContextWithOptions(view.bounds.size, true, 0)
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, 0)
         view.drawViewHierarchyInRect(view.bounds, afterScreenUpdates: true)
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
